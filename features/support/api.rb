a971c65f9f98021fd9c1a64a9e7f4d41b55b3aac
Romeo Ledesma <r_ledesma@outlook.com>
2017-07-16 13:40:50 +0100

    Create an instantiator class

--- features/support/api.rb
+++ features/support/api.rb
@@ -1,5 +1,9 @@
 class API
-  include singleton
+  include Singleton
 
-  
+  def self.method_missing(method_name, *arguments, &block)
+    @@apis ||= Hash.new
+    class_name = method_name.to_s.split('_').collect(&:capitalize).join
+    @@apis[method_name] ||= Object.const_get(class_name).new
+  end
 end
