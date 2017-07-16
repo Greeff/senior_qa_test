798f89cb8ebedc077f795b647664ad31df36a9e6
Romeo Ledesma <r_ledesma@outlook.com>
2017-07-16 13:41:34 +0100

    Use HTTParty. Add credentials validation

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,12 @@
 require 'pry'
 require 'singleton'
-require 'rest_client'
+require 'httparty'
+
+require Dir.pwd + '/features/support/apis/generic.rb'
+
+begin
+  CREDS = {id: ENV['ID'], key: ENV['KEY']}
+ensure
+  raise('Login ID not specified.') if CREDS[:id].nil?
+  raise('API Key not specified.') if CREDS[:key].nil?
+end
